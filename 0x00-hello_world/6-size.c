ba96b0494a28f6ffa8b5f35c0f7af1dfd862dc59
SergioBalca <3896@holbertonschool.com>
2021-09-30 21:10:45 -0700

    program that prints the size of various types on the computer it is compiled and run on, by using printf, variables and type casting

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int a;
+float f;
+
+printf("Size of a char: %lu %s\n", (unsigned long)sizeof(c), "byte(s)");
+printf("Size of int: %lu %s\n", (unsigned long)sizeof(i), "byte(s)");
+printf("Size of long int: %lu %s\n", (unsigned long)sizeof(li), "byte(s)");
+printf("Size of long long int: %lu %s\n", (unsigned long)sizeof(a), "byte(s)");
+printf("Size of float: %lu %s\n", (unsigned long)sizeof(f), "byte(S)");
+return (0);
+}
