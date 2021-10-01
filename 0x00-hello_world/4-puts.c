3dc8a5749d643f35f6059c41c303cc92686428a0
SergioBalca <3896@holbertonschool.com>
2021-09-30 19:14:27 -0700

    a programa that prints a string, using the c function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char text[60] = "Programming is like building a multilingual puzzle";
+
+puts(text);
+return (0);
+
+}
