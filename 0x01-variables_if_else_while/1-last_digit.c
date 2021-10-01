b0591388ad1018ad792875f6ec500cdce0023deb
SergioBalca <3896@holbertonschool.com>
2021-10-01 15:21:32 -0700

    one of the ranges has been changed to integrate negative numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,19 +15,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 ld = n % 10;
 
-if (n < 0)
-{
-ld = (n % 10) * -1;
-}
-else
-{
 if (ld > 5)
 {
 char str5[50] = "and is greater than 5";
 
 printf("Last digit of %d is %d %s\n", n, ld, str5);
 }
-else if (0 < ld && 6 > ld)
+else if (6 > ld && 0 != ld)
 {
 char str6[50] = "and is less than 6 and not 0";
 
@@ -38,7 +32,6 @@ else
 char str0[50] = "and is 0";
 
 printf("Last digit of %d is %d %s\n", n, ld, str0);
-}
 }
 return (0);
 }
