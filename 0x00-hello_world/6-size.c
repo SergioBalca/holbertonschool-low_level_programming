7bd5cc574ef7a42eacb045d6efa9ac89751c1467
SergioBalca <3896@holbertonschool.com>
2021-10-01 10:34:57 -0700

    a variable was defined to avoid the 80 char warning

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,12 @@ int i;
 long int li;
 long long int a;
 float f;
+char t[50] = "byte(s)";
 
 printf("Size of a char: %lu %s\n", (unsigned long)sizeof(c), "byte(s)");
 printf("Size of an int: %lu %s\n", (unsigned long)sizeof(i), "byte(s)");
 printf("Size of a long int: %lu %s\n", (unsigned long)sizeof(li), "byte(s)");
-printf("Size of a long long int: %lu %s\n", (unsigned long)sizeof(a), "byte(s)");
+printf("Size of a long long int: %lu %s\n", (unsigned long)sizeof(a), t);
 printf("Size of a float: %lu %s\n", (unsigned long)sizeof(f), "byte(s)");
 return (0);
 }
