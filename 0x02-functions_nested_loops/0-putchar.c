16aa0bc4bcdc3c43a0f38776312ac3fa1b1a78e7
SergioBalca <3896@holbertonschool.com>
2021-10-04 23:06:03 -0700

    program that prints the word _putchar, without using any C standard function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char input[11] = "_putchar\n";
+int index;
+
+for (index = 0; input[index] != '\0'; index++)
+{
+_putchar(input[index]);
+}
+return (0);
+}
