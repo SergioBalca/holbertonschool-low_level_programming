9d9d1c7766579deb942ee4431bc791827e9e545f
SergioBalca <3896@holbertonschool.com>
2021-10-12 14:59:46 -0700

    a if statement was added to consider strings with an odd number of characters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * puts_half- prints the second half of a string
+ * if length is odd, prints the last character
+ * otherwise, prints the second half of the string
+ * @str: function parameter
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+int count = 0;
+int en = string_length(str) / 2;
+int on = (string_length(str) - 1) / 2;
+
+if (string_length(str) % 2 == 0)
+{
+int count = 0;
+
+while (str[count] != '\0')
+{
+
+while (en <= count)
+{
+_putchar(str[count]);
+en++;
+}
+count++;
+}
+}
+
+else
+{
+while (str[count] != '\0')
+{
+
+while (on <= count)
+{
+_putchar(str[count]);
+on++;
+}
+count++;
+}
+
+}
+_putchar('\n');
+}
+
+/**
+ * string_length- gives the length of a string
+ * @pointer: parameter of the function
+ * Return: the length of the string
+ */
+
+int string_length(char *pointer)
+{
+int c = 0;
+
+while (*(pointer + c) != '\0')
+c++;
+
+return (c);
+}
