079640d309b32e6232ea6cbf0fa2f9263e17135b
SergioBalca <3896@holbertonschool.com>
2021-10-14 11:57:34 -0700

    the scenario where the number of characters in the second string is less than n is considered to avoid errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,6 +12,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
+int k = 0;
 int j = 0;
 
 while (dest[i] != '\0')
@@ -19,6 +20,13 @@ while (dest[i] != '\0')
 i++;
 }
 
+while (src[k] != '\0')
+{
+k++;
+}
+
+if (k >= n)
+{
 while (j < n)
 {
 dest[i] = src[j];
@@ -27,8 +35,19 @@ j++;
 i++;
 }
 
+}
+
+else
+{
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+}
+
 dest[i] = '\0';
 
 return (dest);
-
 }
