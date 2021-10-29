02bc536975ee46c1c0373cc4b414ed1cdcfe0b49
SergioBalca <3896@holbertonschool.com>
2021-10-29 09:28:18 -0700

    function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,38 +14,60 @@ unsigned int i = 0;
 unsigned int j = 0;
 unsigned int size;
 
-if (*s1 == '\0' && *s2 == '\0')
-return ('\0');
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (j >= n)
+if (len(s2) >= n)
 {
-size = i + j + 1;
+size = len(s1) + n + 1;
 }
-size = i + n + 1;
+size = len(s1) + len (s2) + 1;
 ptr = malloc(sizeof(char) * size);
 if (ptr == NULL)
 return ('\0');
+if (s1 != NULL)
+{
 for (i = 0; s1[i] != '\0'; i++)
 {
 ptr[i] = s1[i];
 }
-if (j >= n)
+}
+if (len(s2) >= n)
+{
+if (s2 != NULL)
 {
 for (j = 0; j < n; j++, i++)
 {
 ptr[i] = s2[j];
 }
 }
+}
 else
+{
+if (s2 != NULL)
 {
 for (j = 0; s2[j] != '\0'; j++, i++)
 {
 ptr[i] = s2[j];
 }
 }
+}
 return (ptr);
 }
+/**
+ * string_nconcat- concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of character of s2 to be concatenated
+ * Return: a pointer that points to a new allocated space in memory
+ */
 
+unsigned int len(char *s)
+{
+unsigned int i = 0;
+
+if (s == NULL)
+return (0);
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
