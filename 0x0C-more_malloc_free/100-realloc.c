eb03839a016a3c3ece2f3956baef2effed08a92f
SergioBalca <3896@holbertonschool.com>
2021-10-29 19:45:24 -0700

    function that reallocates a memory block

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc- eallocates a memory block
+ * @ptr: a pointer to the memory previously allocated
+ * @old_size: size, in bytes, of the allocated space for ptr
+ * @new_size: size, in bytes, of new memory block
+ * Return: a pointer to the new memory block
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+ptr = malloc(old_size);
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+}
+if (new_size > old_size)
+{
+ptr = malloc(new_size);
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,6 @@ unsigned int len(char *s2);
 char *_concat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* MAIN_H */
