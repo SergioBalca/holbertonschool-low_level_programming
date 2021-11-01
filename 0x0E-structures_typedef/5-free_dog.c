3bb780ffa940bd94e37615f415cc7c4e827964e5
SergioBalca <3896@holbertonschool.com>
2021-11-01 14:29:59 -0700

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog- frees dogs, meaning that the memory
+ * where the name, age and owner of the dog is freed
+ * @d: pointer to the structure dog that contains the dog data
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+free(d);
+}
