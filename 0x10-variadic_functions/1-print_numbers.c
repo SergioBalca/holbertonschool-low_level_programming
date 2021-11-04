5a146ca666d28c0c55c437263d7ccc9e8db4ce02
SergioBalca <3896@holbertonschool.com>
2021-11-04 09:44:45 -0700

    unction that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * print_numbers- print int numbers
+ * @separator: is the string to be printed between numbers
+ * @n: numbers of integers passesd to the function
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+
+if (n)
+{
+va_list intptr;
+va_start(intptr, n);
+for (; i < n; i++)
+{
+if (!i)
+{
+printf("%d", va_arg(intptr, int));
+}
+else
+{
+if (separator)
+{
+printf("%s %d", separator, va_arg(intptr, int));
+}
+else
+{
+printf("%d ", va_arg(intptr, int));
+}
+}
+}
+printf("%c", '\n');
+}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+#include <stddef.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* MAIN_H */
