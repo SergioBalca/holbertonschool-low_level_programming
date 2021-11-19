674e874edc87d8f7ecd12ac9a09e3ceee79cff66
SergioBalca <3896@holbertonschool.com>
2021-11-18 21:36:21 -0800

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,13 +12,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
-	if (n <= ULONG_MAX || index <= UINT_MAX)
+	if (n >= ULONG_MAX || index >= UINT_MAX)
 	{
-		bit = (n >> index) & 1;
+		return (-1);
 	}
+	
 	else
 	{
-		bit = -1;
+		bit = (n >> index) & 1;
+		return (bit);
 	}
-	return (bit);
 }
