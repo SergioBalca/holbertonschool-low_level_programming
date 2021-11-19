5e00d99d311440ce115fbafe669766a200346c12
SergioBalca <3896@holbertonschool.com>
2021-11-18 20:20:13 -0800

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * get_bit- returns the value of a bit at a given index
+ * @n: decimal number
+ * @index: index of the bit you want to get
+ * Return: the value of the bit at index
+ * or -1 if an error ocurred
  */
 
 int get_bit(unsigned long int n, unsigned int index)
@@ -13,9 +15,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n > ULONG_MAX || index > UINT_MAX)
 	{
 		return (-1);
-	}		
-	
+	}
+
 	bit = (n >> index) & 1;
-	
+
 	return (bit);
 }
