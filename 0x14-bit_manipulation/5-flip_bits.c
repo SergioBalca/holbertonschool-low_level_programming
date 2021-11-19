1b416b97bcddf2b001e3733bd26deb9d2b5c27b0
SergioBalca <3896@holbertonschool.com>
2021-11-19 10:45:01 -0800

    function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count_bit = 0;
 	unsigned int xor = (n ^ m);
 
-	while (xor > 0)
+	while (xor)
 	{
 		count_bit++;
 		xor &= (xor - 1);
