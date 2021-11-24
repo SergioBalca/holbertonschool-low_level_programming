c4a45b1fa5fa591a7747bbabb1ab23adaa68350a
SergioBalca <3896@holbertonschool.com>
2021-11-24 12:16:42 -0800

    function that creates a file. The case where filename == NULL is evaluated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,13 @@ int create_file(const char *filename, char *text_content)
 	int count;
 	ssize_t num_write;
 
+	if (filename == NULL || text_content == NULL)
+	{
+		return (-1);
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (fd == -1 || filename == NULL || text_content == NULL)
+	if (fd == -1)
 	{
 		return (-1);
 
