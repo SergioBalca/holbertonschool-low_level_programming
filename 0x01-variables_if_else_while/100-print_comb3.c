6e1bb5ea943fa8972c8abacd0ecfc0db47c79167
SergioBalca <git@github.com:SergioBalca>
2022-05-28 08:37:15 -0700

    program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			if (a != b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				if (a == 8 && b == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
