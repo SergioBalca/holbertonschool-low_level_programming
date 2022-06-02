304f462fed113e37d721d544eb4cd4da95f65cae
SergioBalca <git@github.com:SergioBalca>
2022-06-02 08:53:58 -0700

    function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,76 +7,124 @@
  */
 void print_number(int n)
 {
-	if (n >= 0)
+
+	/*single digit number*/
+	if (n >= 0 && n < 10)
 	{
-		if (n < 10)
-		{
-			_putchar(n + '0');
-		}
-		else if (n < 100)
-		{
-			/*to print the first digit*/
-			_putchar(((n / 10)) + '0');
-			/*to print second digit*/
-			_putchar((n % 10) + '0');
-		}
-
-		else if (n < 1000)
-		{
-			_putchar((n / 100) + '0');
-			_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10) + '0');
-
-		}
-
-		else
-		{
-			_putchar((n / 1000) + '0');
-			_putchar(((n / 100) % 10) + '0');
-			_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10) + '0');
-		}
+		_putchar(n + '0');
 	}
-	else
-		print_negative_number(n);
-}
-
-/**
- * print_negative_number- prints a negative number passed as argument
- * @n: number to print
- * Return: Nothing
- */
-void print_negative_number(int n)
-{
-
-	n *= -1;
-	if (n < 10)
+	/*single digit negative number*/
+	else if (n < 0 && n > -10)
 	{
 		_putchar('-');
 		_putchar(n + '0');
 	}
-	else if (n < 100)
+	else
 	{
-		_putchar('-');
-		/*to print the first digit*/
-		_putchar(((n / 10)) + '0');
-		/*to print second digit*/
-		_putchar((n % 10) + '0');
+		print(n);
 	}
-	else if (n < 1000)
-	{
-		_putchar('-');
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
+}
 
+/**
+ * number_size- counts the number of digits in an integer
+ * @n: integer
+ * Return: the number of digits of n
+ */
+int number_size(int n)
+{
+	int count = 0;
+
+	if (n > 0)
+	{
+		while (n != 0)
+		{
+			n = n / 10;
+			count++;
+		}
 	}
 	else
 	{
-		_putchar('-');
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
+		n *= -1;
+		while (n != 0)
+		{
+			n = n / 10;
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * print- prints an integer
+ * @n: number to print
+ * Return: Nothing
+ */
+void print(int n)
+{
+	int i = 0;
+	int div = 1;
+	int j = 0;
+	int count = 0;
+
+	count = number_size(n);
+	while (i < count - 1)
+	{
+		div *= 10;
+		i++;
+	}
+
+	if (n > 0)
+	{
+		while (j < count)
+		{
+			if (j == 0)
+				_putchar((n / div) + '0');
+			else
+			{
+				if (div == 0)
+					_putchar((n % 10) + '0');
+				else
+				{
+					_putchar(((n / div) % 10) + '0');
+				}
+			}
+			div /= 10;
+			j++;
+		}
+	}
+	else
+		print_negative(n, div);
+}
+
+
+/**
+ * print_negative- prints a negative integer
+ * @n: number to print
+ * @div: integer to print each digit of n as a character value
+ * Return: Nothing
+ */
+void print_negative(int n, int div)
+{
+	int j = 0;
+	int count = 0;
+
+	count = number_size(n);
+	n *= -1;
+	while (j < count)
+	{
+		if (j == 0)
+		{
+			_putchar('-');
+			_putchar((n / div) + '0');
+		}
+		else
+		{
+			if (div == 0)
+				_putchar((n % 10) + '0');
+			else
+				_putchar(((n / div) % 10) + '0');
+		}
+		div /= 10;
+		j++;
 	}
 }
