e3970cf19c17ec5ca43858ed5d71d2570d921185
SergioBalca <SergioBalca.github.com>
2022-06-11 16:06:59 -0700

    function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int recursive_binary_search(int *arr, size_t s, int v, int low, int high);
-void print_array(int *array, int low, int high);
 
 #endif /* MAIN_H */
